7d1be61a13d032a8fbec84df6be87728d27f5467
zenocoz <federico.soncini@gmail.com>
2021-01-12 16:58:10 +0100

    modified home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,34 @@
-import React, { Component } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { IoBookmarksOutline } from "react-icons/io5";
-import ArticleListItem from "../../components/ArticleListItem/ArticleListItem";
-import Footer from "../../components/Footer/Footer";
-import PeopleList from "../../components/PeopleList/PeopleList";
-import TopicsToFollow from "../../components/TopicsToFollow/TopicsToFollow";
-import articles from "./articles.json";
-import "./styles.scss";
-
+import React, { Component } from "react"
+import { Col, Container, Row } from "react-bootstrap"
+import { IoBookmarksOutline } from "react-icons/io5"
+import ArticleListItem from "../../components/ArticleListItem/ArticleListItem"
+import Footer from "../../components/Footer/Footer"
+import PeopleList from "../../components/PeopleList/PeopleList"
+import TopicsToFollow from "../../components/TopicsToFollow/TopicsToFollow"
+import articles from "./articles.json"
+import "./styles.scss"
 
 export default class Home extends Component {
   state = {
-    articles: articles,
-  };
+    articles: [],
+  }
+
+  fetchArticles = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/articles")
+      if (response.ok) {
+        const articles = await response.json()
+        this.setState({ articles: articles })
+        console.log(articles)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchArticles()
+  }
   render() {
     return (
       <div>
@@ -22,40 +38,44 @@ export default class Home extends Component {
             style={{ borderBottom: "1px solid rgba(230, 230, 230, 1)" }}
           >
             <Col>
-              <ArticleListItem
-                article={this.state.articles[0]}
-                articleImg={"top"}
-                headingFont={"large"}
-                subheading
-              />
+              {this.state.articles.length > 0 && (
+                <ArticleListItem
+                  article={this.state.articles[0]}
+                  articleImg={"top"}
+                  headingFont={"large"}
+                  subheading
+                />
+              )}
             </Col>
 
             <Col className={"flex-column w-100"}>
-              {this.state.articles.slice(1, 5).map((article) => (
-                <ArticleListItem
-                  articleImg={"left"}
-                  headingFont={"small"}
-                  article={article}
-                />
-              ))}
+              {this.state.articles.length > 0 &&
+                this.state.articles.map((article) => (
+                  <ArticleListItem
+                    articleImg={"left"}
+                    headingFont={"small"}
+                    article={article}
+                  />
+                ))}
             </Col>
 
             <Col>
               <PeopleList />
-              <TopicsToFollow/>
+              <TopicsToFollow />
             </Col>
             <Col className={""}>{/*<TagsList />*/}</Col>
           </Row>
           <Row className={"py-4 mt-4"}>
             <Col className={"col-lg-8 pr-5 pl-2"}>
-              {this.state.articles.slice(6).map((article) => (
-                <ArticleListItem
-                  articleImg={"left"}
-                  headingFont={"large"}
-                  subheading
-                  article={article}
-                />
-              ))}
+              {this.state.articles.length > 0 &&
+                this.state.articles.map((article) => (
+                  <ArticleListItem
+                    articleImg={article.category.img}
+                    headingFont={"large"}
+                    subheading
+                    article={article}
+                  />
+                ))}
             </Col>
             <Col className={"col-lg-4 "}>
               <div
@@ -76,6 +96,6 @@ export default class Home extends Component {
           </Row>
         </Container>
       </div>
-    );
+    )
   }
 }
